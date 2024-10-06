7b66cd9798ed6eee447aa1e61249b1a2a01aae2e
= <=>
2024-10-06 12:16:39 +0500

    commit

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,36 +1,37 @@
-'use client';
-import { ThemeProvider } from '@/components/ThemeProvider';
-import React from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { Toaster } from '@/components/ui/toaster';
-const Providers: React.FC<React.PropsWithChildren> = ({ children }) => {
-  const [queryClient] = React.useState<QueryClient>(
+"use client";
+import { ThemeProvider } from "@/components/ThemeProvider";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "@/components/ui/toaster";
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(
     () =>
       new QueryClient({
         defaultOptions: {
           queries: {
+            // With SSR, we usually want to set some default staleTime
+            // above 0 to avoid refetching immediately on the client
             staleTime: 60 * 1000 * 5,
           },
         },
       })
   );
+
   return (
-    <>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        <Toaster />
-        <QueryClientProvider client={queryClient}>
-          {children}
-          <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-      </ThemeProvider>
-    </>
+    <ThemeProvider
+      attribute='class'
+      defaultTheme='system'
+      enableSystem
+      disableTransitionOnChange
+    >
+      <Toaster />
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 };
-
 export default Providers;
