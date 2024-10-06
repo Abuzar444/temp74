de7b0fce087d90a3705021ee97202a2c3401df82
= <=>
2024-10-06 13:10:56 +0500

    "Remove type annotation for QueryClient in useState"

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "@/components/ui/toaster";
 const Providers: React.FC<React.PropsWithChildren> = ({ children }) => {
-  const [queryClient] = React.useState<QueryClient>(
+  const [queryClient] = React.useState(
     () =>
       new QueryClient({
         defaultOptions: {
