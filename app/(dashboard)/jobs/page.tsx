5a453ce2566eefa08cdf6a5e59d0f7943f07a4d7
= <=>
2024-10-06 11:58:26 +0500

    push

--- app/(dashboard)/jobs/page.tsx
+++ app/(dashboard)/jobs/page.tsx
@@ -1,24 +1,4 @@
-import React from "react";
-import JobsList from "@/components/JobsList";
-import SearchForm from "@/components/SearchForm";
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query";
-import { getAllJobsAction } from "@/utils/actions";
-const Jobs: React.FC = async () => {
-  const queryClient = new QueryClient();
-  await queryClient.prefetchQuery({
-    queryKey: ["jobs", "", "all", 1],
-    queryFn: () => getAllJobsAction({}),
-  });
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <SearchForm />
-      <JobsList />
-    </HydrationBoundary>
-  );
+const page = () => {
+  return <div>page</div>;
 };
-
-export default Jobs;
+export default page;
