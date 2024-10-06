8b26ad6041f12224120bcbcd0e0be513896522f3
= <=>
2024-10-06 11:47:40 +0500

    first commit

---
+++ app/(dashboard)/jobs/loading.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from "react";
+
+const loading: React.FC = () => {
+  return (
+    <div className='loading loading-spinner text-primary loading-lg'></div>
+  );
+};
+
+export default loading;
