3c60b22b7fd59756621a481f9ce04c2017918fb9
= <=>
2024-10-06 14:08:17 +0500

    Remove unused react-query imports from JobDetailPage and Stats page components.

--- app/(dashboard)/jobs/[id]/page.tsx
+++ app/(dashboard)/jobs/[id]/page.tsx
@@ -1,12 +1,6 @@
 import EditJobForm from "@/components/EditJobForm";
 import { getSingleJobAction } from "@/utils/actions";
 
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query";
-
 async function JobDetailPage({ params }: { params: { id: string } }) {
   return <EditJobForm jobId={params.id} />;
 }
--- app/(dashboard)/stats/page.tsx
+++ app/(dashboard)/stats/page.tsx
@@ -2,11 +2,6 @@ import React from "react";
 import ChartsContainer from "@/components/ChartsContainer";
 import StatsContainer from "@/components/StatsContainer";
 import { getChartsDataAction, getStatsAction } from "@/utils/actions";
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query";
 
 const Stats = async () => {
   return (
