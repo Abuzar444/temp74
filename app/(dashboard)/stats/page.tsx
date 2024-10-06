6bbbb49edd7edd889199874c4ed57c459b6b9136
= <=>
2024-10-06 12:00:14 +0500

    commit

--- app/(dashboard)/stats/page.tsx
+++ app/(dashboard)/stats/page.tsx
@@ -1,30 +1,4 @@
-import React from "react";
-import ChartsContainer from "@/components/ChartsContainer";
-import StatsContainer from "@/components/StatsContainer";
-import { getChartsDataAction, getStatsAction } from "@/utils/actions";
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query";
-
-const Stats = async () => {
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery({
-    queryKey: ["stats"],
-    queryFn: () => getStatsAction(),
-  });
-  await queryClient.prefetchQuery({
-    queryKey: ["charts"],
-    queryFn: () => getChartsDataAction(),
-  });
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <StatsContainer />
-      <ChartsContainer />
-    </HydrationBoundary>
-  );
+const page = () => {
+  return <div>page</div>;
 };
-
-export default Stats;
+export default page;
